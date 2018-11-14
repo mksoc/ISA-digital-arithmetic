94746eac474685b28640525ce5ddfa1d4d7fbaec
StMiky <st.miky77@gmail.com>
2018-11-14 11:56:30 +0100

    Updated version0_add1mult2 legs and reports

--- HW_filter/sim/py-sim-script.tcl
+++ HW_filter/sim/py-sim-script.tcl
@@ -1,27 +1,27 @@
 # =================================================================
 # *****************************************************************
 # *************** GENERATED USING sim_script_gen.py ***************
-# ************           2018-11-13 13:11:25           ************
+# ************           2018-11-13 13:26:13           ************
 # *****************************************************************
 # The script was generated starting from these values of variables:
 # run_remote = 1
 # cli_mode = 1
 # version = 0
-# design = 1
+# design = 2
 # =================================================================
 
 puts "Running in command line mode. No waveforms will be available."
 
 # compile source files
-vcom -93 -work ./work ../src/*.vhd
-vcom -93 -work ./work ../version0/*.vhd
+vcom -93 -work ./work ../src/filter_pkg.vhd
+vlog -work ./work ../version0/*.v
 
 # compile testbench
 vcom -93 -work ./work ../tb/*.vhd
-vlog -work ./work +define+NO_GUI=1 ../tb/iir_filterTB.v
+vlog -work ./work +define+SYN=1+NO_GUI=1 ../tb/iir_filterTB.v
 
 # load design
-vsim    work.iir_filterTB
+vsim -L /software/dk/nangate45/verilog/msim6.2g -sdftyp /iir_filterTB/UUT=../version0/iir_filter.sdf -pli /software/synopsys/syn_current/auxx/syn/power/vpower/lib-linux/libvpower.so   work.iir_filterTB
 
 # run simulation
 run -all
