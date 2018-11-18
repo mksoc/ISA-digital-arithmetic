cb2779663d94bf21d37562573be6c333a452ccaa
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2018-11-18 13:16:03 +0100

    Finished testing approx compressor

--- HW_filter/version3/approx_comp_4to2.vhd
+++ HW_filter/version3/approx_comp_4to2.vhd
@@ -1,18 +1,17 @@
--- file '4to2_approx_comp.vhd'
 -- approximate 4 to 2 compressor proposed by Yin
 -- 1 positive error and 7 negative errors
 -- out of 16 output combinations
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity 4to2_approx_comp is
-  port (
-    input: in std_logic_vector(3 downto 0);
-    output: out std_logic_vector(1 downto 0)
-  ) ;
-end 4to2_approx_comp;
+entity approx_comp_4to2 is
+    port (
+        input: in std_logic_vector(3 downto 0);
+        output: out std_logic_vector(1 downto 0)
+    ) ;
+end approx_comp_4to2;
 
-architecture structure of 4to2_approx_comp is
+architecture structure of approx_comp_4to2 is
 begin
     -- 'sum' bit
     output(0) <= not(input(3) xor input(2));
