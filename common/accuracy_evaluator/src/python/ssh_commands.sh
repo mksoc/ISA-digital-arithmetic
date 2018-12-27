d7528a57e4058e8dba9bc6d9477d2e8ef2747ae9
mp-17 <twisted@live.it>
2018-12-27 17:51:05 +0100

    added report for version1 and version3. Result analysis missing

--- common/accuracy_evaluator/src/python/ssh_commands.sh
+++
@@ -1,6 +0,0 @@
-cd /home/isa22/lab2/syn
-		source /software/scripts/init_synopsys
-		mkdir work logs reports saif netlist
-		dc_shell-xg-t -f synth-filter.tcl
-		mv netlist/* ../version3
-		mv saif/NangateOpenCellLibrary.saif ../sim
